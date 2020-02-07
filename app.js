2b8c8d9f8099cc93252f25418a11538b9dbd93b4
unknown <stoichevrado@gmail.com>
2020-02-07 23:30:28 +0200

    update 1.3

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://rst92:'+ process.env.MONGO_ATLAS_PW +'@node-restapi-shop-hi8xf.mongodb.net/test?retryWrites=true&w=majority', {
+    useMongoClient: true
+})
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
